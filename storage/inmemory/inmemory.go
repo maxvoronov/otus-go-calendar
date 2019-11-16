7b8d7c8acb41526c117e367144b3f20e6aade937
Max <v0id@list.ru>
2019-11-16 23:47:11 +0200

    Add mutex to in-memory storage

--- storage/inmemory/inmemory.go
+++ storage/inmemory/inmemory.go
@@ -2,18 +2,22 @@ package inmemory
 
 import (
 	"github.com/maxvoronov/otus-go-calendar/entity"
+	"sync"
 )
 
 type InMemoryStorage struct {
+	sync.Mutex
 	events map[string]*entity.Event
 }
 
 func NewInMemoryStorage() *InMemoryStorage {
 	eventStorage := make(map[string]*entity.Event)
-	return &InMemoryStorage{eventStorage}
+	return &InMemoryStorage{events: eventStorage}
 }
 
 func (storage *InMemoryStorage) GetAll() ([]*entity.Event, error) {
+	storage.Mutex.Lock()
+	defer storage.Mutex.Unlock()
 	result := make([]*entity.Event, 0, len(storage.events))
 	for _, event := range storage.events {
 		result = append(result, event)
@@ -23,6 +27,8 @@ func (storage *InMemoryStorage) GetAll() ([]*entity.Event, error) {
 }
 
 func (storage *InMemoryStorage) GetById(id string) (*entity.Event, error) {
+	storage.Mutex.Lock()
+	defer storage.Mutex.Unlock()
 	if event, ok := storage.events[id]; ok {
 		return event, nil
 	}
@@ -31,12 +37,16 @@ func (storage *InMemoryStorage) GetById(id string) (*entity.Event, error) {
 }
 
 func (storage *InMemoryStorage) Save(event *entity.Event) error {
+	storage.Mutex.Lock()
+	defer storage.Mutex.Unlock()
 	storage.events[event.Id.String()] = event
 
 	return nil
 }
 
 func (storage *InMemoryStorage) Remove(event *entity.Event) error {
+	storage.Mutex.Lock()
+	defer storage.Mutex.Unlock()
 	delete(storage.events, event.Id.String())
 
 	return nil
